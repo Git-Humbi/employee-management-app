cd8f82dca179cd3af9eaca64c7d62708f5f3c462
Ian Githumbi <iangithumbi1975@gmail.com>
2026-03-28 22:30:08 +0300

    completed the new-contact.html page and main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function handleEdit() {
+    alert("Employee record editing mode activated (Placeholder).");
+}
+
+function handleDelete() {
+    const confirmation = confirm("Are you sure you want to delete?");
+    if (confirmation) {
+        alert("Record successfully deleted (visual confirmation).");
+    }
+}
